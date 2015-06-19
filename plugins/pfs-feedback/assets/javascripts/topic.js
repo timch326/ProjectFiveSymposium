5b80eec1394b3b4f966578f7695a3c2eacf97bbf
Tim C <timch326@gmail.com>
2015-06-19 11:18:13 -0700

    Feedback plugin UI now mostly works.

--- plugins/pfs-feedback/assets/javascripts/topic.js
+++ plugins/pfs-feedback/assets/javascripts/topic.js
@@ -1,15 +1,9 @@
 Discourse.Topic.reopen({
   toggleFeedback: function(feedbackType) {
-      var topic = this;
-
       return Discourse.ajax({
       url: this.get('url') + '/toggle_feedback',
       type: 'PUT',
       data: { feedback_type: feedbackType}
-    }).then(function (response) {
-        console.log(response);
-        topic.set('feedback', response.feedback);
-        topic.set('feedback_votes', response.feedback_votes);
     });
   }
 });
